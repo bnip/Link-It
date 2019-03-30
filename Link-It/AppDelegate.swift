2f579618c368f57e3c0a79c4325c5d749ffa3366
Bryan Nip <bryannip@buffalo.edu>
2019-03-30 18:29:33 -0400

    Initial Commit

---
+++ Link-It/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Link-It
+//
+//  Created by Kerbal on 3/30/19.
+//  Copyright © 2019 bryannip. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Link-It/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Link-It
+//
+//  Created by Kerbal on 3/30/19.
+//  Copyright © 2019 bryannip. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
