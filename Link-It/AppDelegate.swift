133bb22ec70e3be7200ab887636508dae2323b09
Bryan Nip <bryannip@buffalo.edu>
2019-03-30 20:15:54 -0400

    Added status bar item and menu

--- Link-It/AppDelegate.swift
+++ Link-It/AppDelegate.swift
@@ -11,16 +11,30 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    var item: NSStatusItem? = nil
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        item = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        item?.button?.title = "Link It"
+        
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Link It!", action: #selector(AppDelegate.linkIt), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quit), keyEquivalent: ""))
+        
+        item?.menu = menu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    @objc func linkIt() {
+        print("Pressed")
+    }
+    
+    @objc func quit() {
+        NSApplication.shared.terminate(self)
+    }
 }
 
